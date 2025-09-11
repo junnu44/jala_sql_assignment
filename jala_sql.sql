873fc9932536213eb2c6c0de2a5f1b3b313f71ef
Juned Sheikh <junaidsheikhjs2003@gmail.com>
2025-09-11 17:51:21 +0530

    Updated Assignment

--- jala_sql.sql
+++ jala_sql.sql
@@ -1,3 +1,4 @@
+
 --1.	Display snum,sname,city and comm of all salespeople.
 SELECT SNUM, SNAME, CITY, COMM FROM Salespeople;
 --2.	Display all snum without duplicates from all orders.
@@ -28,13 +29,35 @@ SELECT * FROM Cust WHERE CITY IS NULL;
 SELECT * FROM Orders WHERE ODATE IN ('1994-10-03', '1994-10-04');
 --15.	All customers serviced by peel or Motika.
 SELECT * FROM Cust WHERE SNUM IN (SELECT SNUM FROM Salespeople WHERE SNAME IN ('Peel','Motika'));
---15a.	All customers whose names begin with a letter from A to B.
+--16.	All customers whose names begin with a letter from A to B.
 SELECT * FROM Cust WHERE CNAME LIKE 'A%' OR CNAME LIKE 'B%';
---16.	All orders except those with 0 or NULL value in amt field.
+--17.	All orders except those with 0 or NULL value in amt field.
 SELECT * FROM Orders WHERE AMT IS NOT NULL AND AMT <> 0;
---17.	Count the number of salespeople currently listing orders in the order table.
+--18.	Count the number of salespeople currently listing orders in the order table.
 SELECT COUNT(DISTINCT SNUM) AS total_salespeople FROM Orders;
---18.	Largest order taken by each salesperson, datewise.
+--19.	Largest order taken by each salesperson, datewise.
 SELECT SNUM, ODATE, MAX(AMT) AS largest_order FROM Orders GROUP BY SNUM, ODATE ORDER BY SNUM, ODATE;
---19.	Largest order taken by each salesperson with order value more than $3000.
+--20.	Largest order taken by each salesperson with order value more than $3000.
 SELECT SNUM, MAX(AMT) FROM Orders GROUP BY SNUM HAVING MAX(AMT) > 3000;
+--21.	Which day had the hightest total amount ordered.
+SELECT ODATE, SUM(AMT) AS total_amount FROM Orders GROUP BY ODATE ORDER BY total_amount DESC LIMIT 1;
+--22.	Count all orders for Oct 3rd.
+SELECT COUNT(*) AS total_orders FROM Orders WHERE ODATE = '1994-10-03'
+--23.	Count the number of different non NULL city values in customers table.
+SELECT COUNT (DISTINCT CITY) AS total_unique_cities FROM Cust WHERE CITY IS NOT NULL;
+--24.	Select each customer’s smallest order.
+SELECT CNUM, MIN(AMT) AS small_order FROM Orders GROUP BY CNUM;
+--25.	First customer in alphabetical order whose name begins with G.
+SELECT * FROM Cust WHERE CNAME LIKE 'G%' ORDER BY CNAME ASC LIMIT 1;
+--26.	Get the output like “ For dd/mm/yy there are ___ orders.
+SELECT TO_CHAR(ODATE, 'dd/mm/yy') AS order_date, 'For ' || TO_CHAR(ODATE, 'DD/MM/YY') || ' there are ' || COUNT(*) || ' orders' AS result
+FROM Orders GROUP BY ODATE ORDER BY ODATE;
+--27.	Assume that each salesperson has a 12% commission. Produce order no., salesperson no., and amount of salesperson’s commission for that order.
+SELECT ONUM, SNUM, AMT * 0.12 AS comission_ammount FROM Orders;
+--28.	Find highest rating in each city. Put the output in this form. For the city (city), the highest rating is : (rating).
+SELECT 'For the city ' || CITY || ', the highest rating is : ' || MAX(RATING) AS result FROM Cust GROUP BY CITY;
+--29.	Display the totals of orders for each day and place the results in descending order.
+SELECT ODATE, SUM(AMT) AS total_amount FROM Orders GROUP BY ODATE ORDER BY total_amount DESC;
+--30.	All combinations of salespeople and customers who shared a city. (ie same city).
+SELECT s.SNAME AS salesperson, c.CNAME AS customer, s.CITY FROM Salespeople s JOIN Cust c ON s.CITY = c.CITY ORDER BY s.CITY, s.SNAME, c.CNAME;
+ 
